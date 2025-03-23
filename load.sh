103ec869bcfbdb5c3525c231ff45b54416016732
sharmajii7 <153850833+sharmajii7@users.noreply.github.com>
2025-03-23 12:42:30 -0400

    Update load.sh

--- load.sh
+++ load.sh
@@ -1,1 +1,12 @@
-while true; do curl http://localhost:5000/simulate_load; echo ""; done
+# while true; do curl http://localhost:5000/simulate_load; echo ""; done
+count=0
+while true; do
+  curl http://localhost:5000/simulate_load
+  echo ""
+  count=$((count + 1))
+  if [ "$count" -eq 5 ]; then
+    sudo bash assignment3.sh &
+    # count=0  # Reset count if you want to repeat the process
+  fi
+  # echo ""
+done
